021118fed50f172313833b9eced1b16fbf51d516
Karanrhe <karanveer.singh@rheem.com.au>
2025-06-13 15:38:06 +1000

    Ex 152

---
+++ APP-1/Coding Exercise/GUI_convertor.py
@@ -0,0 +1,12 @@
+import FreeSimpleGUI as sg
+
+label1 = sg.Text("Enter feet:")
+label2 = sg.Text("Enter inches:")
+user_input1 = sg.Input()
+user_input2 = sg.Input()
+button1 = sg.Button("Convert")
+
+window = sg.Window("Convertor", layout=([label1, user_input1],[label2, user_input2], [button1]))
+
+window.read()
+window.close()
